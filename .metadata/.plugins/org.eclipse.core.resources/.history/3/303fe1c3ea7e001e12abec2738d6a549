72dfef616a5651cd391de57449b44d925f7c1e8c
Giovanni Pace <braintechinnovations@gmail.com>
2023-11-09 12:13:50 +0100

    Aggiunti controlli semplici

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3/303fe1c3ea7e001e12abec2738d6a549
@@ -0,0 +1,39 @@
+package its.lez00;
+
+public class Stringhe {
+
+	public static void main(String[] args) {
+
+//		System.out.println("Ciao Giovanni");
+		
+		//------ Concatenazione
+		
+//		System.out.println("Ciao" + "Valeria");
+		
+//		System.out.println("Il mio numero preferito è: " + 7);
+//		System.out.println("Il mio numero preferito è: " + "7");
+//		System.out.println(7 + " è il mio numero preferito");
+		
+		//-----------------------------------------------------------
+		
+//		System.out.println(5 + 7);
+//		System.out.println("5" + "7");
+		
+//		System.out.println("Il mio numero preferito è: " + 7 + 3);	//Comportamento anomalo
+//		System.out.println(7 + 3 + " è il mio num preferito");
+		
+//		System.out.println("Il mio numero preferito è: " + (7 + 3));
+
+		//-----------------------------------------------------------
+
+		int a = 5;
+		double k = 65.4d;
+		
+		System.out.println("I valori richiesti sono: \na: " + a + "\nk: " + k);
+		
+		
+		
+		
+	}
+
+}
---
+++ CorsoITSoop/src/its/lez01/controlli/ControlliSemplici.java
@@ -0,0 +1,126 @@
+package its.lez01.controlli;
+
+public class ControlliSemplici {
+
+	public static void main(String[] args) {
+
+		/*
+		 		if(condizione){
+		 			//Sottoprogramma in caso positivo
+		 		}
+		 		else{
+		 			//Sottoprogramma in caso negativo
+		 		}
+		 */
+		
+//		System.out.println("Inizio programma");
+//		
+//		if(false) {
+//			System.out.println("Sono nel caso positivo");
+//		}
+//		else {
+//			System.out.println("SOno nel caso negativo");
+//		}
+//		
+//		System.out.println("Fine programma");
+		
+		//------------------------------------------------
+		
+		// > 	Maggiore
+        // >= 	Maggiore o uguale
+        // < 	Minore
+        // <=	Minore o uguale
+        // ==	Uguale a
+        // !=	Diverso da
+		
+//		System.out.println(5 != 3);
+		
+		//------------------------------------------------
+
+//		int eta = -18;
+//		
+//		if(eta >= 18) {
+//			System.out.println("Sei maggiorenne");
+//		} 
+//		else {
+//			System.out.println("Sei minorenne");
+//		}
+		
+		//------------------------------------------------
+
+//		int eta = 15;
+//		
+//		if(eta > 0) {
+//			if(eta < 130) {
+//				if(eta >= 18) {
+//					System.out.println("Sei maggiorenne");
+//				} 
+//				else {
+//					System.out.println("Sei minorenne");
+//				}
+//			}
+//			else {
+//				System.out.println("Errore nell'età");
+//			}
+//		}
+//		else {
+//			System.out.println("Errore nell'età");
+//		}
+		
+		//------------------------------------------------
+
+		//AND
+		
+		//int eta = 15;
+		//  TRUE   &&   TRUE        = TRUE
+		//    1     *      1        = 1
+		
+		//int eta = -15;
+		//  FALSE  && TRUE          = FALSE
+		//      0   *    1          = 0
+		
+//		int eta = 240;
+//		//   TRUE  &&  FALSE        = FALSE
+//		//	    1   *      0        = 0
+//		if(eta > 0 && eta < 130) {
+//			System.out.println("Controllo maggiorenne minorenne");
+//		}
+//		else {
+//			System.out.println("Errore nell'età");
+//		}
+		
+		//------------------------------------------------
+
+		//OR
+		
+		//int eta = -19;
+		//  TRUE    ||    FALSE       = TRUE
+		//     1     +        0       = 1
+		
+		//int eta = 240;
+		//   FALSE  ||    TRUE        =  TRUE
+		//       0   +       1        = 1
+		
+		int eta = 19;
+		//   FALSE	||    FALSE		= FALSE
+		//       0   +        0     = 0
+		if(eta <= 0 || eta >= 130) {
+			System.out.println("Errore nell'età");
+		}
+		else {
+			System.out.println("Controllo maggiorenne minorenne");
+		}
+		
+		 /*
+	     * TODO: ESERCIZIO!
+	     * Scrivere da zero una funzione che, data la temperatura corporea mi dice se posso entrare
+	     * in un locale stando attenti all'input che non deve essere inferiore di 35° e superiore a 42°
+	     *
+	     * La temperatura massima di ingresso è di 37.5°
+	     *
+	     * Possibili output sono: "Puoi entrare" | "Non puoi entrare"
+	     */
+		
+	}
+
+}
